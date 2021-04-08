bebfaba87f1201771f90a06ecf6a573f8c648135
Zheng <ahzzheng@gmail.com>
2021-04-07 18:16:44 -0700

    Prevent DoujinInformation from breaking when image is too large, update Podfile to work on M1 computers

--- SauceFinder/View/DoujinViews/DoujinInformation.swift
+++ SauceFinder/View/DoujinViews/DoujinInformation.swift
@@ -12,80 +12,70 @@ struct DoujinInformation: View {
     @Environment(\.presentationMode) var presentationMode
     var theAPI:DoujinAPI
     var doujinModel: DoujinInfoViewModel
-
     
     var body: some View {
-        GeometryReader{ geo in
-            ScrollView(.vertical){
-                ZStack{
-                    
-                    Button(action: {
-                        alertShow.toggle()
-                    }) {
-                        Image(systemName: "trash.circle")
-                            .resizable()
-                            .foregroundColor(.red)
-                            .frame(width:25, height: 25)
+        ScrollView {
+            
+            VStack {
+                
+                Text(doujinModel.name)
+                    .font(.title)
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.center)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding([.leading, .trailing,.top], 50)
+                
+                
+                Text("Sauce:\(doujinModel.id)")
+                    .font(.title2)
+                
+                Text("\(doujinModel.numPages) pages")
+                    .font(.title2)
+                    .padding(.bottom, 25)
+                
+                Text("Tags")
+                
+                TagView(tagArray: doujinModel.doujinTags)
+                    .padding(.bottom, 150)
+                    .padding([.trailing, .leading], 10)
+                
+                Spacer()
+                
+                Image(uiImage: convertBase64ToImage(doujinModel.pictureString))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .padding(.horizontal, 50)
+                
+                
+            }
+            .overlay(
+                VStack {
+                    HStack {
+                        Button(action: {
+                            alertShow.toggle()
+                        }) {
+                            Image(systemName: "trash.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width:25, height: 25)
+                        }
+                        .padding(10)
                         
+                        Spacer()
                     }
-                    .offset(x:-160, y:-235)
-                    
-                    
-                    VStack{
-                        Group{
-                            
-                            Text(doujinModel.name)
-                                .font(.title)
-                                .fontWeight(.regular)
-                                .multilineTextAlignment(.center)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .frame(alignment:.center)
-                                .padding([.leading, .trailing,.top], 50)
-                            //                            .font(.headline)
-                            
-                            
-                            
-                            Text("Sauce:\(doujinModel.id)")
-                                .font(.title2)
-//                                .padding(.bottom, 25)
-                            
-                            Text("\(doujinModel.numPages) pages")
-                                .font(.title2)
-                                .padding(.bottom, 25)
-                            
-                            
-                            Text("Tags")
-                                .frame(alignment: .center)
-                            
-                            TagView(tagArray: doujinModel.doujinTags)
-                                .padding(.bottom, 150)
-                                .padding([.trailing, .leading], 10)
-                            
-                            
-                            Spacer()
-                                                        
-                            ImageView(image: convertBase64ToImage(doujinModel.pictureString))
-                                .frame(alignment:.center)
-                                .padding([.leading, .trailing], 50)
-                            
-                            
-                            
-                            
-                        }
-                        //This will ask the user if they would like to delete the current entry
-                        .alert(isPresented: $alertShow){
-                            Alert(title: Text("Would you like to Delete this entry"),message: Text(doujinModel.name),primaryButton:
-                                    .default(Text("Delete")) {
-                                        alertShow.toggle()
-                                        doujinModel.deleting = true
-                                        presentationMode.wrappedValue.dismiss()
-                                    }, secondaryButton: .cancel())
-                        }
-                    }
+                    Spacer()
                 }
+            )
+            //This will ask the user if they would like to delete the current entry
+            .alert(isPresented: $alertShow){
+                Alert(title: Text("Would you like to Delete this entry"),message: Text(doujinModel.name),primaryButton:
+                        .default(Text("Delete")) {
+                            alertShow.toggle()
+                            doujinModel.deleting = true
+                            presentationMode.wrappedValue.dismiss()
+                        }, secondaryButton: .cancel())
             }
         }
-        .frame(alignment:.center)
     }
 }
 
